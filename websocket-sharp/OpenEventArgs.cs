fcb553bfe44f60bf7126122156b820709e67670c
zhangyazhou <zhangyazhou@xylink.com>
2022-03-14 14:04:39 +0800

    引入 websocket sharp

---
+++ websocket-sharp/OpenEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebSocketSharp
+{
+    public class OpenEventArgs : EventArgs
+    {
+        public string LocalIP { get; private set; }
+        public string RemoteIP { get; private set; }
+        public OpenEventArgs(string localIp, string remoteIp)
+        {
+            LocalIP = localIp;
+            RemoteIP = remoteIp;
+        }
+    }
+}
