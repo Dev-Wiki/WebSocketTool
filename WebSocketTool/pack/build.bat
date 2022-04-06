2058b1db15672f2043adfc9a9807cc171577a8e9
DevWiki <devwiki@qq.com>
2022-04-06 22:17:04 +0800

    修复打包问题

---
+++ WebSocketTool/pack/build.bat
@@ -0,0 +1,32 @@
+set version=1.0.0
+
+if not "%~1"=="" (
+    set version=%1
+)
+
+cd ../ && rmdir /s /q bin && rmdir /s /q obj 
+cd pack
+
+devenv ../../WebSocketTool.sln /ReBuild "Release|AnyCPU" /project WebSocketTool
+
+if NOT %errorlevel%==0 @goto :FailureOnBuild
+
+libz.exe inject-dll --assembly ../bin/Release/WebSocketTool.exe --include ../bin/Release/*.dll --move
+
+if not exist "output" (
+    mkdir output
+)
+
+cd output
+if exist "WebSocketTool-%version%.exe" (
+    del /q /s WebSocketTool-%version%.exe
+)
+cd ../
+
+cd ../bin/Release && move WebSocketTool.exe ../../pack/output/WebSocketTool-%version%.exe 
+cd ../ && del /q /s Release && cd ../pack
+
+goto :eof
+
+:FailureOnBuild
+echo build project failure
--- WebSocketTool/pack/pack.bat
+++
@@ -1,4 +0,0 @@
-libz.exe inject-dll --assembly ../bin/Release/WebSocketTool.exe --include ../bin/Release/*.dll --move
-
-mkdir output
-move ../bin/Release/WebSocketTool.exe output/WebSocketTool.exe
